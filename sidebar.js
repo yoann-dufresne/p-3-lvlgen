77c6ef42ed87b31227718c21152cfcb782397355
yoann-dufresne <yoann.dufresne0@gmail.com>
2021-05-27 21:17:17 +0200

    rm lvl ok

--- sidebar.js
+++ sidebar.js
@@ -1,4 +1,5 @@
 
+
 let set_triggers = function() {
 	let left = document.querySelector("#left_bar");
 	let adder_div = document.querySelector("#lvl_adder");
@@ -13,7 +14,13 @@ let set_triggers = function() {
 		elem.classList.add("lvl_summary");
 		elem.style["min-height"] = "50px";
 		elem.style.width = "100%";
+		elem.innerHTML = '<button class="up_lvl"> up </button><button class="down_lvl">down</button><button class="del_lvl">delete</button>';
 		lvl_list.appendChild(elem);
+		
+		elem.querySelector(".del_lvl").onclick = function (e) {
+			let parent = e.target.parentNode;
+			lvl_list.removeChild(parent);
+		}
 	}
 };
 
